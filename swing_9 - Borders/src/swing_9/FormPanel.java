11029bfff113f1f6e12250c6ab8f235e56006659
CJ O'Sullivan <me@cjosullivan.net>
2018-01-20 14:46:09 +0000

    Added borders to the tool bar and form pane.

---
+++ swing_9 - Borders/src/swing_9/FormPanel.java
@@ -0,0 +1,28 @@
+package swing_9;
+
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+public class FormPanel extends JPanel {
+	
+	// Constructor 
+	public FormPanel() {
+		
+		// Method called getPreferredSize returns and object called a dimension 
+		Dimension dim = getPreferredSize();
+		dim.width = 250;
+		setPreferredSize(dim);
+		
+		// Two borders that will be combined to create one
+		Border innerBorder = BorderFactory.createTitledBorder("Add Person");
+		Border outerBorder = BorderFactory.createEmptyBorder(5,5,5,5);
+		
+		// Combining the borders 
+		setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
+		
+	}
+
+}
