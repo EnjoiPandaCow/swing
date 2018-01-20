344714ea8fb997e6922635674d0a8b745822b13a
CJ O'Sullivan <me@cjosullivan.net>
2018-01-20 14:10:49 +0000

    Used listeners to properly de couple the custom components of the app.

---
+++ swing_7 - Listeners/src/swing_7/App.java
@@ -0,0 +1,23 @@
+package swing_7;
+
+import javax.swing.SwingUtilities;
+
+import swing_7.MainFrame;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		// Explanation in Section 1 Lecture 2 
+		SwingUtilities.invokeLater(new Runnable() {
+
+			public void run() {
+				
+				new MainFrame();
+				
+			}
+		});
+		
+	}
+
+}
